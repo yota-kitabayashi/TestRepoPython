cafc23f7a4790e8522e24c99aa17482e9844a66e
yota.kitabayashi <yota.kitabayashi@yotakitabayashinoMacBook-Pro.local>
2018-06-26 00:52:15 +0900

    second commit

--- getTimelines.py
+++ getTimelines.py
@@ -10,7 +10,7 @@ twitter = OAuth1Session(CK, CS, AT, ATS) #認証処理
 
 url = "https://api.twitter.com/1.1/statuses/home_timeline.json" #タイムライン取得エンドポイント
 
-params ={'count' : 10} #取得数
+params ={'count' : 5} #取得数
 res = twitter.get(url, params = params)
 
 if res.status_code == 200: #正常通信出来た場合
