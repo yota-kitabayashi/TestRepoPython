6de31e3066e4e81bba36d007aab50400cd32615d
北林 洋太 <yota.kitabayashi@BIZ0015000009.local>
2018-06-27 18:20:58 +0900

    Add SearchKeyword

---
+++ searchKeywords.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+import json, config #標準のjsonモジュールとconfig.pyの読み込み
+from requests_oauthlib import OAuth1Session #OAuthのライブラリの読み込み
+
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+ATS = config.ACCESS_TOKEN_SECRET
+twitter = OAuth1Session(CK, CS, AT, ATS) #認証処理
+
+url = "https://api.twitter.com/1.1/search/tweets.json"
+
+print("何を調べますか?")
+keyword = raw_input('>> ')
+print('----------------------------------------------------')
+
+
+params = {'q' : keyword, 'count' : 10}
+
+req = twitter.get(url, params = params)
+
+if req.status_code == 200:
+    search_timeline = json.loads(req.text)
+    for tweet in search_timeline['statuses']:
+        print(tweet['user']['name'] + '::' + tweet['text'])
+        print(tweet['created_at'])
+        print('----------------------------------------------------')
+else:
+    print("ERROR: %d" % req.status_code)
